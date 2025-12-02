b2c38e1369053b9eafbd2bfeeb83f7272db67ea3
Sjoerd Visscher <sjoerd.visscher@tweag.io>
2025-12-02 10:29:35 +0100

    Day 2

--- aoc2.hs
+++ aoc2.hs
@@ -1,3 +1,17 @@
+import Data.List.Split (splitOn)
 
 main :: IO ()
-main = interact id
+main = interact (show . sum . map count . parse)
+
+parse :: String -> [(Integer, Integer)]
+parse = map ((\[a,b] -> (read a, read b)) . splitOn "-") . splitOn ","
+
+count :: (Integer, Integer) -> Integer
+count (l, h) = sum [n | n <- [l..h], test2 (show n)]
+
+test1 :: String -> Bool
+test1 s = let (a, b) = splitAt (length s `div` 2) s in a == b
+
+test2 :: String -> Bool
+test2 s = let l = length s in
+  or [ take l (cycle (take i s)) == s | i <- [1..l `div` 2], l `mod` i == 0]
