fbbbae5682a80b8872b754183a425d757bb1971d
Sjoerd Visscher <sjoerd.visscher@tweag.io>
2025-12-04 13:50:01 +0100

    Day 3

--- aoc3.hs
+++ aoc3.hs
@@ -1,3 +1,12 @@
+import Data.List (elemIndex)
+import Data.Maybe (fromJust)
 
 main :: IO ()
-main = interact id
+main = interact (show . sum . map (solve . map (read . pure)) . lines)
+
+solve :: [Int] -> Int
+solve = go 12 0
+  where
+    go 0 acc _ = acc
+    go n acc bs = let m = maximum (take (length bs - n + 1) bs) in
+      go (n - 1) (acc * 10 + m) (drop (fromJust (elemIndex m bs) + 1) bs)
