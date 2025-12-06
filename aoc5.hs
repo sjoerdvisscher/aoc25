617f8e2d4fc9a521643557c3e10c782f858de581
Sjoerd Visscher <sjoerd.visscher@iohk.io>
2025-12-06 08:34:02 +0100

    Day 5

--- aoc5.hs
+++ aoc5.hs
@@ -1,3 +1,18 @@
+import Data.Bifunctor (bimap)
+import Data.List.Split (splitOn)
+import Data.Interval ((<=..<=), width)
+import Data.IntervalSet ( Extended(Finite), fromList, toList )
 
 main :: IO ()
-main = interact id
+main = interact (show . solve2 . parse)
+
+parse :: String -> ([(Int, Int)], [Int])
+parse s =
+  let [ranges, ids] = splitOn "\n\n" s
+  in (map (bimap read (read . drop 1) . break (== '-')) (lines ranges), map read (lines ids))
+
+solve1 :: ([(Int, Int)], [Int]) -> Int
+solve1 (ranges, ids) = length $ filter (\i -> any (\(l, h) -> l <= i && i <= h) ranges) ids
+
+solve2 :: ([(Int, Int)], [Int]) -> Int
+solve2 = sum . map (succ . width) . toList . fromList .  map (\(l, h) -> Finite l <=..<= Finite h) . fst
