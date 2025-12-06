02ecf8789630d592d67a17ab3a2ffaf7d5658dc1
Sjoerd Visscher <sjoerd.visscher@iohk.io>
2025-12-06 08:34:09 +0100

    Day 6

--- aoc6.hs
+++ aoc6.hs
@@ -1,3 +1,15 @@
+import Data.List (transpose)
+import Data.List.Split (splitOn)
+import Debug.Trace (traceShowId)
 
 main :: IO ()
-main = interact id
+main = interact (show . sum . map calc . fix2 . lines)
+
+fix1 :: [String] -> [[String]]
+fix1 = transpose . map words
+
+fix2 :: [String] -> [[String]]
+fix2 ss = zipWith (\op args -> args ++ [op]) (words (last ss)) . map (map head) . splitOn [[]] . map words $ transpose (init ss)
+
+calc :: [String] -> Int
+calc ss = (if last ss == "+" then sum else product) (map read $ init ss)
