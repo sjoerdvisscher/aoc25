25b763952e8f974776db5b80599a0511dfa3e649
Sjoerd Visscher <sjoerd.visscher@iohk.io>
2025-12-07 09:15:34 +0100

    Day 7

--- aoc6.hs
+++ aoc6.hs
@@ -1,15 +1,14 @@
 import Data.List (transpose)
 import Data.List.Split (splitOn)
-import Debug.Trace (traceShowId)
 
 main :: IO ()
 main = interact (show . sum . map calc . fix2 . lines)
 
-fix1 :: [String] -> [[String]]
-fix1 = transpose . map words
+fix1 :: [String] -> [(String, [String])]
+fix1 = map (\ss -> (last ss, init ss)) . transpose . map words
 
-fix2 :: [String] -> [[String]]
-fix2 ss = zipWith (\op args -> args ++ [op]) (words (last ss)) . map (map head) . splitOn [[]] . map words $ transpose (init ss)
+fix2 :: [String] -> [(String, [String])]
+fix2 ss = zip (words (last ss)) . map (map head) . splitOn [[]] . map words $ transpose (init ss)
 
-calc :: [String] -> Int
-calc ss = (if last ss == "+" then sum else product) (map read $ init ss)
+calc :: (String, [String]) -> Int
+calc (op, ss) = (if op == "+" then sum else product) (map read ss)
--- aoc7.hs
+++ aoc7.hs
@@ -1,3 +1,27 @@
+import Data.IntSet (IntSet)
+import Data.IntSet qualified as IS
+import Data.Map qualified as Map
 
 main :: IO ()
-main = interact id
+main = interact (show . solve2 . map parse . lines)
+
+parse :: [Char] -> IntSet
+parse = IS.fromList . map fst . filter ((/= '.') . snd) . zip [0..]
+
+solve1 :: [IntSet] -> Int
+solve1 [] = 0
+solve1 (start:pss) = go start pss 0
+  where
+    go _ [] acc = acc
+    go ps (sps:spss) acc = go ((ps IS.\\ hits) `IS.union` (IS.map pred hits) `IS.union` (IS.map succ hits)) spss (acc + IS.size hits)
+      where
+        hits = ps `IS.intersection` sps
+
+solve2 :: [IntSet] -> Int
+solve2 [] = 0
+solve2 (start:pss) = go start' pss
+  where
+    start' = Map.fromList ((, 1::Int) <$> IS.toList start)
+    go ps [] = sum (Map.elems ps)
+    go ps (sps:spss) = go (Map.unionsWith (+) $ Map.mapWithKey (split sps) ps) spss
+    split sps k v = Map.fromList $ if k `IS.member` sps then [(k - 1, v), (k + 1, v)] else [(k, v)]
