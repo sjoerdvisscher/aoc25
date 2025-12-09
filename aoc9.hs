88359a4031785f4d7cdb3eaca62c88da51fcc564
Sjoerd Visscher <sjoerd.visscher@iohk.io>
2025-12-09 11:46:15 +0100

    Day 9

--- aoc9.hs
+++ aoc9.hs
@@ -1,3 +1,11 @@
-
 main :: IO ()
-main = interact id
+main = interact (show . solve . map (\s -> read ("(" ++ s ++ ")")) . lines)
+
+solve :: [(Integer, Integer)] -> Integer
+solve ps = maximum [(x2 - x1 + 1) * (y2 - y1 + 1)
+  | (xa, ya) <- ps, (xb, yb) <- ps, let x1 = min xa xb, let y1 = min ya yb, let x2 = max xa xb, let y2 = max ya yb
+  , all (\(x, y) -> x <= x1 || x >= x2 || y <= y1 || y >= y2) testPoints]
+  where
+    ps' = ps ++ take 1 ps
+    mids = zipWith (\(x1, y1) (x2, y2) -> ((x1 + x2) `div` 2, (y1 + y2) `div` 2)) ps' (drop 1 ps')
+    testPoints = ps ++ mids
