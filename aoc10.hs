b2d99b01b4c312d572323445619eefb8c95fdc07
Sjoerd Visscher <sjoerd.visscher@iohk.io>
2025-12-12 11:02:18 +0100

    Day 12

--- aoc10.hs
+++ aoc10.hs
@@ -2,10 +2,11 @@ import Data.Bits
 import Data.List (transpose)
 import Data.SBV
 import System.IO.Unsafe (unsafePerformIO)
-import Debug.Trace (traceShowId)
+import Debug.Trace (traceShowId, traceShow)
+import Data.Set qualified as Set
 
 main :: IO ()
-main = interact (show . sum . map (solve1 . parse . words) . lines)
+main = interact (show . sum . map (solve2' . parse . words) . lines)
 
 parse :: [String] -> (Int, [[Int]], [Int])
 parse ss = (parseTarget (head ss), map parseList (chop ss), parseList (last ss))
@@ -29,3 +30,18 @@ solve2 (_, buttons, js) = traceShowId $ minimum $ map sum res
       xs <- mkFreeVars (length buttons)
       let rowEq row r = sum (zipWith (*) xs row) .== r
       solve $ zipWith rowEq (transpose buttons') (map (literal . fromIntegral) js) ++ map (.>= 0) xs
+
+-- implementation of https://www.reddit.com/r/adventofcode/comments/1pk87hl/2025_day_10_part_2_bifurcate_your_way_to_victory/
+solve2' :: (Int, [[Int]], [Int]) -> Int
+solve2' (_, buttons, js) = traceShowId $ go (Set.singleton (0, js, 1))
+  where
+    go set = if all (== 0) r then c else go (set' <> Set.fromList opts)
+      where
+        ((c, r, f), set') = Set.deleteFindMin set
+        opts = [ (c + popCount i * f, map (`div` 2) r', f * 2)
+          | i::Int <- [0..bit (length buttons) - 1]
+          , let bs = foldMap snd . filter (testBit i . fst) . zip [0..] $ buttons
+          , let r' = map (\(k, j) -> j - length (filter (== k) bs)) (zip [0..] r)
+          , all even r'
+          , all (>= 0) r'
+          ]
--- aoc12.hs
+++ aoc12.hs
@@ -1,3 +1,23 @@
+import Data.Set (Set)
+import Data.Set qualified as Set
+import Data.List.Split (splitOn)
 
 main :: IO ()
-main = interact id
+main = interact (show . solve . parse)
+
+type V2 = (Int, Int)
+parse :: String -> ([Set V2], [(V2, [Int])])
+parse s = (map (parseShape . tail) (init parts), map parseRegion (last parts))
+  where
+    parts = splitOn [""] $ lines s
+    parseShape sh = Set.fromList [ (x, y) | (y, l) <- zip [0..] sh, (x , c) <- zip [0..] l, c == '#']
+    parseRegion r = ((read w, read h), map read cs)
+      where
+        (d:cs) = words r
+        [w,h] = splitOn ("x") (init d)
+
+solve :: ([Set V2], [(V2, [Int])]) -> Int
+solve (shapes, regions) = sum (map (\(d, cs) -> fromEnum $ solve1 (zip shapes cs) d) regions)
+
+solve1 :: [(Set V2, Int)] -> V2 -> Bool
+solve1 shapes (w, h) = w * h >= sum (map (\(sh, c) -> c * Set.size sh) shapes)
